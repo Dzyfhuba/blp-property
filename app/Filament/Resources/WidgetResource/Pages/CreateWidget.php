554817c33fcb34071aaff0c9caba8c8e1945e222
Dzyfhuba <hafidz21ub@gmail.com>
2024-02-01 23:20:14 +0700

    add widget

---
+++ app/Filament/Resources/WidgetResource/Pages/CreateWidget.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\WidgetResource\Pages;
+
+use App\Filament\Resources\WidgetResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateWidget extends CreateRecord
+{
+    protected static string $resource = WidgetResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return '/admin/widgets';
+    }
+
+
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -10,7 +12,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable 
+class User extends Authenticatable implements FilamentUser
 // implements MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles;
@@ -45,4 +47,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
 }
