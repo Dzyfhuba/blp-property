d48e2678287efa771462fd287d6fbb232c7ae2dd
Dzyfhuba <hafidz21ub@gmail.com>
2024-03-17 16:58:54 +0700

    add products display

--- resources/js/Components/ProductsDisplay.tsx
+++ resources/js/Components/ProductsDisplay.tsx
@@ -23,15 +23,15 @@ const ProductsDisplay = () => {
 
   return (
     <section id="products-display" className={style.section}>
-      <div className="divider divider-primary">
+      <div className="divider divider-primary h-24 sm:h-auto">
         <h2 className={style.title}>Properti-properti kami</h2>
       </div>
 
       <div className={style.gridContainer}>
         {products.map((product, idx) => (
           <div key={idx}>
-            <div className='mask mask-circle text-base-content bg-base-200 p-3'>
-              <IoIosHome size={32} className='mx-auto' />
+            <div className='mask mask-circle text-base-content bg-base-200 p-5'>
+              <IoIosHome size={52} className='mx-auto' />
             </div>
             <h3>{product.title}</h3>
             <span>{product.subtitle}</span>
