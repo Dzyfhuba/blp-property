ae4b6c0c5854f03bfb82dcd9338cb2ed4a3b65f2
Dzyfhuba <hafidz21ub@gmail.com>
2024-03-17 16:43:31 +0700

    add section: our properties

---
+++ resources/js/Components/ProductsDisplay.tsx
@@ -0,0 +1,45 @@
+import { IoIosHome } from 'react-icons/io'
+import style from './ProductsDisplay.module.css'
+
+const ProductsDisplay = () => {
+  const products = [
+    {
+      title: 'GKB',
+      subtitle: 'Gresik Kota Baru'
+    },
+    {
+      title: 'PPS',
+      subtitle: 'Pondok Permata Suci'
+    },
+    {
+      title: 'GKR',
+      subtitle: 'Gresik Kota Raya'
+    },
+    {
+      title: 'PSR',
+      subtitle: 'Permata Sidayu Residence'
+    },
+  ]
+
+  return (
+    <section id="products-display" className={style.section}>
+      <div className="divider divider-primary">
+        <h2 className={style.title}>Properti-properti kami</h2>
+      </div>
+
+      <div className={style.gridContainer}>
+        {products.map((product, idx) => (
+          <div key={idx}>
+            <div className='mask mask-circle text-base-content bg-base-200 p-3'>
+              <IoIosHome size={32} className='mx-auto' />
+            </div>
+            <h3>{product.title}</h3>
+            <span>{product.subtitle}</span>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default ProductsDisplay
--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,14 +1,14 @@
-import { getImageUrl } from '@/Helpers/url'
+import Heros from '@/Components/Heros'
+import ProductsDisplay from '@/Components/ProductsDisplay'
 import Layout from '@/Layouts/Layout'
 import { PageProps } from '@/types'
-import Brand from '../Images/brand.png'
-import Heros from '@/Components/Heros'
 
 export default function Dashboard(props: PageProps) {
   console.log(props.widgets.filter(a => a.layout?.includes('header')))
   return (
     <Layout widgets={props.widgets}>
       <Heros />
+      <ProductsDisplay />
     </Layout>
   )
 }
