ddb9207d806e5efacbbf528f100fa5f4c871077a
Dzyfhuba <hafidz21ub@gmail.com>
2024-04-17 14:48:50 +0700

    fix: update search engine

--- app/Http/Controllers/API/SearchController.php
+++ app/Http/Controllers/API/SearchController.php
@@ -15,37 +15,14 @@ class SearchController extends Controller
 {
     public function index(Request $request)
     {
-        // change this can break algorithm
-        $designOptions = DesignOption::query()->orderByDesc('value')->get();
-        $facilityOptions = FacilityOption::query()->orderByDesc('value')->get();
-        $locationOptions = LocationOption::query()->orderByDesc('value')->get();
-        $publicFacilityOptions = PublicFacilityOption::query()->orderByDesc('value')->get();
-        $bedrooms = Product::query()
-            ->distinct('bedrooms')->orderByDesc('bedrooms')
-            ->selectRaw('bedrooms as value, concat(bedrooms," kamar tidur") as label')->get();
-        $bathrooms = Product::query()
-            ->distinct('bathrooms')->orderByDesc('bathrooms')
-            ->selectRaw('bathrooms as value, concat(bathrooms," kamar mandi") as label')->get();
-        $floors = Product::query()
-            ->distinct('floors')->orderByDesc('floors')
-            ->selectRaw('floors as value, concat(floors," lantai") as label')->get();
-
+        // get all criterion ratings and map it, so it can suit with search engine form
         $criterionRatings = CriterionRating::all()->mapWithKeys(fn($item, $idx) => [
-            $item['criteria'] => array_map(fn ($subitem) => [
+            $item['criteria'] => array_map(fn($subitem) => [
                 'label' => "{$subitem['label']}: {$subitem['range']}",
                 'value' => $subitem['value']
             ], $item['rating'])
         ]);
 
-        // return response([
-        //     'designOptions' => $designOptions,
-        //     'facilityOptions' => $facilityOptions,
-        //     'locationOptions' => $locationOptions,
-        //     'publicFacilityOptions' => $publicFacilityOptions,
-        //     'bedrooms' => $bedrooms,
-        //     'bathrooms' => $bathrooms,
-        //     'floors' => $floors,
-        // ]);
         return response($criterionRatings);
     }
 }
